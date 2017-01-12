151165d91696f79286dfad389dffb76295f0670b
Oscar Duran <xoscarduranx@gmail.com>
2017-01-12 21:18:09 +0100

    config repository.php

--- config/repository.php
+++ config/repository.php
@@ -231,7 +231,7 @@ return [
             'presenters'   => 'Presenters',
             'validators'   => 'Validators',
             'controllers'  => 'Http/Controllers',
-            'provider'     => 'RepositoryServiceProvider',
+            'provider'     => 'paymentServiceProvider',
             'criteria'     => 'Criteria',
 
         ]
